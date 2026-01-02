26be436f162b994b56b95fa4ea5ddfec7f710095
mpearc11 <mpearc11@jhu.edu>
2026-01-02 14:56:20 -0500

    Update prediction-matrix-streamlit.py

--- prediction-matrix-streamlit.py
+++ prediction-matrix-streamlit.py
@@ -17,9 +17,9 @@ st.title('Prediction Matrix')
 
 st.header('Input PID, ConScore, and FoldScore Values')
 
-pid = st.text_input('enter your target PID')
-conscore = st.text_input('enter your target ConScore')
-foldscore = st.text_input('enter your target FoldScore')
+pid = int(st.text_input('enter your target PID'))
+conscore = int(st.text_input('enter your target ConScore'))
+foldscore = int(st.text_input('enter your target FoldScore'))
 
 if pid <= 100 and pid >= 90:
   pid_guide = A
